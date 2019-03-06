291dbb06b95adc900b86cf280373c36825e376c2
koduki <pascalm3-git@gmail.com>
2019-03-05 22:55:52 -0800

    Add a support of SQL errors

--- problem/app.rb
+++ problem/app.rb
@@ -2,13 +2,22 @@ require 'sqlite3'
 require 'json'
 
 sql = ENV['SQL']
+status = 0
 
 db = SQLite3::Database.new("/app/test.db")
-columns, *rows = db.execute2(sql)
-db.close
+begin
+    columns, *rows = db.execute2(sql)
+rescue Exception => ex
+    message = ex.message
+    status = 1
+ensure
+    db.close 
+end
 
 response = {
-    "status" => 0,
-    "result" => [columns] + rows
+    "status" => status,
+    "message" => message,
+    "result" => (status == 0) ? [columns] + rows : nil
 }
 puts JSON.generate(response)
+
